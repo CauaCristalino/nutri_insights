81ab5c82f1ad219dc3c498c90b24bd8cd49aa8ee
CauaCristalino <caua.pereira345@gmail.com>
2026-06-10 13:50:33 -0300

    Adiciona estrutura inicial do Projeto

---
+++ src/schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS nutri_insights;
+
+USE nutri_insights;
+
+CREATE TABLE IF NOT EXISTS alimentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_alimento VARCHAR(200),
+    categoria_alimento VARCHAR(255),
+    pais VARCHAR(100),
+    calorias_100g FLOAT,
+    proteinas_100g FLOAT,
+    gorduras_100g FLOAT,
+    gordura_sat_100g FLOAT,
+    carboidratos_100g FLOAT,
+    acucar_100g FLOAT,
+    sodio_100g FLOAT,
+    fibras_100g FLOAT,
+    inserido_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
